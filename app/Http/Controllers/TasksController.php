09516d5ef636cd1fd9518060863024d29309192e
naoyan <happynaoyan112@gmail.com>
2021-01-19 06:54:01 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -51,7 +51,7 @@ class TasksController extends Controller
             'content' => 'required|max:10',
             ]);
             
-        $task = Task::findOrFail(id);
+        $task = Task::findOrFail($id);
         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
