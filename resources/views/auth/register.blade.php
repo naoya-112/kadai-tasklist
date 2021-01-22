90eae25637f22c3789483c7b2846e2f5b55fb5e5
naoyan <happynaoyan112@gmail.com>
2021-01-22 07:10:08 +0000

    user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,6 +15,11 @@
                 </div>
                 
                  <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+                
+                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
