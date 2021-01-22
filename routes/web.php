4291b1acd9b41758f1e7edcb4b572f5f6948a8a7
naoyan <happynaoyan112@gmail.com>
2021-01-22 08:01:07 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
-
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController');
 
 //　認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -24,3 +24,4 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+});
