d00b4960cb59ec757baae18e36dff7787dc2556c
naoyan <happynaoyan112@gmail.com>
2021-01-22 06:28:38 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Tasklist</a>
+        <a class="navbar-brand" href="/">Tasks</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -10,28 +10,10 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                @if (Auth::check())
-                    {{-- ユーザ一覧ページへのリンク --}}
-                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
-                    <li class="nav-item dropdown">
-                        <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item"><a href="#">My profile</a></li>
-                            <li class="dropdown-divider"></li>
-                            {{-- ログアウトへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                        </ul>
-                    </li>
-                @else
-                    <ul class="nav navbar-nav navbar-right">
-                        {{-- ユーザ登録ページへのリンク --}}
-                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                        {{-- ログインページへのリンク --}}
-                        <li><a href="#">Login</a></li>
-                    </ul>    
-                @endif
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
         </div>
     </nav>
